6b98c91e418e66dc4e5913979709f442407ffd89
lealle <yysi00558800@gmail.com>
2026-01-13 18:07:19 +0900

    [fix/#1] 라우팅 문법오류 수정

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -12,7 +12,7 @@ class AppRoutes {
     static const String root = '/';
   static const String login = '/login';
   static const String register = '/register';
-  static const String home = '/home';
+  static const String homeView = '/home';
   static const String search = '/search';
   static const String storage = '/storage';
   static const String mypage = '/mypage';
